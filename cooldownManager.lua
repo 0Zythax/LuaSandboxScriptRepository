303ae24993a2695107e3f00685536dd79782ba68
Zythax <Zythax@tutamail.com>
2025-10-02 21:05:59 +0000

    Update cooldownManager.lua

--- cooldownManager.lua
+++ cooldownManager.lua
@@ -1,58 +1,35 @@
--- probably could have made this better but oh well
-local playerCooldownManager = {};
-playerCooldownManager.__index = playerCooldownManager;
+local cooldown = {};
+cooldown.__index = cooldown;
 
-local staticCooldownManager = {};
-staticCooldownManager.__index = staticCooldownManager;
-
-local cooldownManager = {};
-cooldownManager.__index = cooldownManager;
-
-function cooldownManager.new(type : string)
-	local tbl = type == "player" and playerCooldownManager or staticCooldownManager;
-	local self = setmetatable({}, tbl);
+function cooldown.new()
+	local self = setmetatable({}, cooldown);
 	self.cooldowns = {};
 	return self;
 end
 
--- static
-
-function staticCooldownManager:add(item : string, time : number)
-	self.cooldowns[item] = tick() + time;
-end
-
-function staticCooldownManager:onCooldown(item : string)
-	if self.cooldowns[item] == nil or self.cooldowns[item] < tick() then
-		if self.cooldowns[item] then
-			self.cooldowns[item] = nil;
-		end
-		return false;
+function cooldown:addCooldown(player : Player, identifier : string, _time : number)
+	if not self.cooldowns[player] then
+		self.cooldowns[player] = {};
 	end
-	return true;
+	self.cooldowns[player][identifier] = tick() + _time;
 end
 
--- player
-
-function playerCooldownManager:register(player : Player)
-	self.cooldowns[player] = {};
+function cooldown:isOnCooldown(player : Player, identifier : string)
+	local onCooldown = false;
+	if self.cooldowns[player] ~= nil and self.cooldowns[player][identifier] ~= nil then
+		if self.cooldowns[player][identifier] > tick() then
+			onCooldown = true;
+		else
+			self.cooldowns[player][identifier] = nil;
+		end
+	end
+	return onCooldown;
 end
 
-function playerCooldownManager:deregister(player : Player)
-	if self.cooldowns[player] then
+function cooldown:deregister(player : Player)
+	if self.cooldowns[player] ~= nil then
 		self.cooldowns[player] = nil;
 	end
 end
 
-function playerCooldownManager:isOnCooldown(player : Player, item : string)
-	if self.cooldowns[player] == nil then return false end;
-	
-	if self.cooldowns[player][item] == nil or self.cooldowns[player][item] < tick() then
-		if self.cooldowns[player][item] then
-			self.cooldowns[player][item] = nil;
-		end
-		return false;
-	end
-	return true;
-end
-
-return cooldownManager;
+return cooldown;
